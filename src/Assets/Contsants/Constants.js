597f90b9d647207c4d8c63909c3d7d52d2064c9c
ramchandra3101 <ramchandra3101@gmail.com>
2025-02-26 18:55:19 -0500

    updating new portfolio

--- src/Assets/Contsants/Constants.js
+++
@@ -1,4 +0,0 @@
-export const MAPS_URL =
-  "https://www.hartford21.com/?rcstdid=MQ%3d%3d-WOzw8Oa5P2c%3d&gadid=517842344767&network=g&device=c&keyword=downtown%20hartford%20apartments&adgroup=123191108082&campaign=12885739608&gad_source=1&gclid=Cj0KCQjw_sq2BhCUARIsAIVqmQsNJKnHm5X0isDl9ggHIHg53bJgzW5wyYTfMj-tqplbgM2rk8Qcj9kaAuKmEALw_wcB";
-export const RESUME_URL =
-  "https://github.com/ramchandra3101/Resume_SDE/blob/main/RamchandraSDE.pdf";
--- src/components/pages/Blog.js
+++
@@ -1,4 +0,0 @@
-const Blog = () => {
-  return <div></div>;
-};
-export default Blog;
